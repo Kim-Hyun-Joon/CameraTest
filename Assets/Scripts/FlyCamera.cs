dab94f3e10e399a8adb34f902c1628f34af6dd81
김 현준 <jkajks123456@einssnc.com>
2021-06-28 17:51:21 +0900

    Camera Sample Test

---
+++ Assets/Scripts/FlyCamera.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlyCamera : MonoBehaviour {
+    //wasd : basic movement
+
+    public float mainSpeed = 10.0f; //regular speed
+    public float shiftAdd = 250.0f; //multiplied by how long shift is held.  Basically running
+    public float maxShift = 1000.0f; //Maximum speed when holding shift
+    public float camSens = 0.25f; //How sensitive it with mouse
+    
+    private Vector3 lastMouse = new Vector3(255, 255, 255); //kind of in the middle of the screen, rather than at the top (play)
+    private float totalRun = 1.0f;
+
+    void Update() {
+        if (Input.GetMouseButton(0)) {
+            lastMouse = Input.mousePosition - lastMouse;
+            lastMouse = new Vector3(-lastMouse.y * camSens, lastMouse.x * camSens, 0);
+            lastMouse = new Vector3(transform.eulerAngles.x + lastMouse.x, transform.eulerAngles.y + lastMouse.y, 0);
+            transform.eulerAngles = lastMouse;
+            lastMouse = Input.mousePosition;
+            //Mouse  camera angle done.  
+        }
+        //Keyboard commands
+        Vector3 p = GetBaseInput();
+        if (p.sqrMagnitude > 0) { // only move while a direction key is pressed
+            totalRun = Mathf.Clamp(totalRun * 0.5f, 1f, 1000f);
+            p = p * mainSpeed;
+            p = p * Time.deltaTime;
+            Vector3 newPosition = transform.position;
+            transform.Translate(p);
+        }
+    }
+
+    private Vector3 GetBaseInput() { //returns the basic values, if it's 0 than it's not active.
+        Vector3 p_Velocity;
+        /*
+        if (Input.GetKey(KeyCode.W)) {
+            p_Velocity += new Vector3(0, 0, 1);
+        }
+        if (Input.GetKey(KeyCode.S)) {
+            p_Velocity += new Vector3(0, 0, -1);
+        }
+        if (Input.GetKey(KeyCode.A)) {
+            p_Velocity += new Vector3(-1, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.D)) {
+            p_Velocity += new Vector3(1, 0, 0);
+        }
+        */
+        p_Velocity = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), 0);
+        return p_Velocity;
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 20;
+    private Vector3 motion;
+    private Rigidbody rb;
+
+    private void Start() {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update() {
+        motion = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+        rb.velocity = motion * speed;
+    }
+}
